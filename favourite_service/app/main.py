51496f8e19b3ae885cd2fca808874523c450430a
Lobanov <137296664+LobanSel@users.noreply.github.com>
2024-04-03 14:57:20 +0300

    9-10

--- favourite_service/app/main.py
+++ favourite_service/app/main.py
@@ -1,8 +1,8 @@
 import os
 import uvicorn
-from fastapi import FastAPI, Depends, HTTPException, status, Query
+from fastapi import FastAPI, Depends, HTTPException, status, Form, Header
 from typing import Annotated
-
+from keycloak import KeycloakOpenID
 from sqlalchemy.orm import Session
 
 from database import favourites_database as database
@@ -24,9 +24,47 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
+KEYCLOAK_URL = "http://keycloak:8080/"
+KEYCLOAK_CLIENT_ID = "testClient"
+KEYCLOAK_REALM = "testRealm"
+KEYCLOAK_CLIENT_SECRET = "**********"
+
+keycloak_openid = KeycloakOpenID(server_url=KEYCLOAK_URL,
+                                  client_id=KEYCLOAK_CLIENT_ID,
+                                  realm_name=KEYCLOAK_REALM,
+                                  client_secret_key=KEYCLOAK_CLIENT_SECRET)
+
+from prometheus_fastapi_instrumentator import Instrumentator
+Instrumentator().instrument(app).expose(app)
+
+@app.post("/get_jwt_token")
+async def login(username: str = Form(...), password: str = Form(...)):
+    try:
+        token = keycloak_openid.token(grant_type=["password"],
+                                      username=username,
+                                      password=password)
+        global user_token
+        user_token = token
+        return token
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=400, detail="Не удалось получить токен")
+
+def chech_for_role_test(token):
+    try:
+        token_info = keycloak_openid.introspect(token)
+        if "test" not in token_info["realm_access"]["roles"]:
+            raise HTTPException(status_code=403, detail="Access denied")
+        return token_info
+    except Exception as e:
+        raise HTTPException(status_code=401, detail="Invalid token or access denied")
+
 @app.get("/health", status_code=status.HTTP_200_OK)
-async def service_alive():
-    return {'message': 'service alive'}
+async def service_alive(token: str = Header()):
+    if (chech_for_role_test(token)):
+        return {'message': 'service alive'}
+    else:
+        raise HTTPException(status_code=401, detail="Invalid token or access denied")
 
 @app.post("/add_favourite")
 async def add_favourite(name: str, url: str, db: db_dependency):
