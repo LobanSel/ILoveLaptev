c0434b0278adc7f2a6d6bc8c8bd92caaac5b26ca
LobanSel <137296664+LobanSel@users.noreply.github.com>
2024-04-04 10:30:13 +0300

    Update test_integration.py

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -1,23 +1,9 @@
-import pytest
-from pathlib import Path
-import sys
+import requests 
 
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
-
-sys.path.append(str(BASE_DIR))
-sys.path.append(str(BASE_DIR / 'movies_service/app'))
-sys.path.append(str(BASE_DIR / 'favourite_service/app'))
-
-from movies_service.app.main import service_alive as movies_status
-from favourite_service.app.main import service_alive as favourite_status
-
-
-@pytest.mark.asyncio
 async def test_movies_service_connection():
-    r = await movies_status()
+    r = requests.get("http://localhost:8000/health")
     assert r == {'message': 'service alive'}
 
-@pytest.mark.asyncio
 async def test_favourite_service_connection():
-    r = await favourite_status()
+    r = requests.get("http://localhost:8001/health")
     assert r == {'message': 'service alive'}
